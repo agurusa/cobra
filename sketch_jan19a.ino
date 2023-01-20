6423e2eb4c8fd63a53eb6be4e1b5229581c0cddf
Aarthi Gurusami <aarthi.gurusami@gmail.com>
2023-01-20 10:53:08 -0800

    work out button func bug
    .

--- sketch_jan19a.ino
+++ sketch_jan19a.ino
@@ -2,32 +2,40 @@
 
 
 // used to indicate which button was pressed
-enum Button {
+enum class Button {
   mode,
   comms, 
   none
-}
+};
 
 // used to indicate how long a button was pressed
-enum PressTime {
-  long,
-  short
+enum class PressTime {
+  longPress,
+  shortPress
+};
+// debug
+template <typename Enumeration>
+auto as_integer(Enumeration const value)
+    -> typename std::underlying_type<Enumeration>::type
+{
+    return static_cast<typename std::underlying_type<Enumeration>::type>(value);
 }
 
-ezButton modeButton(21);
-ezButton commsButton(22);
+const int MODE_BUTTON_PIN = 23;
+const int COMMS_BUTTON_PIN = 22;
+
+// initialize button pins as input. Use the internal pullup resistor.
+ezButton modeButton(MODE_BUTTON_PIN);
+ezButton commsButton(COMMS_BUTTON_PIN);
 
 const int DEBOUNCE_TIME = 50; // ms
 const int LONG_PRESS_TIME = 1000; // ms
-Button lastPressedButton = Button.none; // used to track long button presses
+Button lastPressedButton = Button::none; // used to track long button presses
 unsigned int lastPressedTime = 0; // keeps track of how long a button has been pressed
 int pressTime = 0; // difference between the last pressed time and the current time 
 
 void setup() {
   Serial.begin(9600); // for debugging
-  // initialize button pins as input. Use the internal pullup resistor.
-  pinMode(modeButtonPin, INPUT_PULLUP);
-  pinMode(commsButtonPin, INPUT_PULLUP);
   modeButton.setDebounceTime(DEBOUNCE_TIME);
   commsButton.setDebounceTime(DEBOUNCE_TIME);
 
@@ -35,19 +43,32 @@ void setup() {
 
 void pressModeButton(PressTime pressTime){
   // if long press
+  if (pressTime == PressTime::longPress){
   // power off if on, power on if off
-  // else
+    Serial.println("long press on press mode button: powering off/on");
+  }
+  else{
   // change mode
+    Serial.println("short press on press mode button: changing mode");
+  }
+
 
 }
 
 void pressCommsButton(PressTime pressTime){
   // send out comms, depending on the current mode
+  if (pressTime == PressTime::longPress){
+    Serial.println("long press on comms mode button");
+  }
+  else
+  {
+    Serial.println("short press on comms mode button: send out comms");
+  }
 }
 
 void pressButton(Button btn, PressTime pressTime) {
   // perform the action associated with the button press
-  if (btn == Button.mode) {
+  if (btn == Button::mode) {
     pressModeButton(pressTime);    
   }
   else {
@@ -58,20 +79,24 @@ void pressButton(Button btn, PressTime pressTime) {
 Button getButtonState()
 {
   // returns the Button that is pressed. If no button is pressed, will return Button.none
-  modeButtonState = modeButton.isPressed()
-  commsButtonState = commsButton.isPressed()
-  if (modeButton.isPressed()) {
-    return Button.mode;
+  // note: we can't use isPressed() because that only relays the *change* between HIGH and LOW
+  // states. 
+  bool modeButtonPressed = modeButton.getState() == 0;
+  bool commsButtonPressed = commsButton.getState() == 0;
+  if (modeButtonPressed) {
+    return Button::mode;
   }
-  else if (commsButton.isPressed()) {
-    return Button.comms;
+  else if (commsButtonPressed) {
+    return Button::comms;
   }
   else {
-    return Button.none;
+    return Button::none;
   }
 }
 
   void loop() {
+    // this speeds up the simulation
+    delay(10);
     // necessary button setup for ezbutton library
     modeButton.loop();
     commsButton.loop();
@@ -79,30 +104,26 @@ Button getButtonState()
     Button pressedButton = getButtonState();
     // if a button is currently pressed, see how long that button has been pressed.
     // if this is the first moment the button is pressed, start the clock.
-    // otherwise, see if enough time has passed to count it as a "long press."
-    if (pressedButton != Button.none) {
+    if (pressedButton != Button::none) {
       if (pressedButton != lastPressedButton){
         lastPressedTime = millis(); 
-      }
-      else{
-        pressTime = millis() - lastPressedTime;
-        if (pressTime > LONG_PRESS_TIME){
-          pressButton(pressedButton, PressTime.long);   
-        }
       }
     }
     // if no buttons are pressed now, but a button *was* pressed without signaling
     // the long press time, consider this a short press of the previously pressed button.
-    else if (pressedButton == Button.none && lastPressedButton != Button.none) {
+    else if (pressedButton == Button::none && lastPressedButton != Button::none) {
+      pressTime = millis() - lastPressedTime;
       if (pressTime < LONG_PRESS_TIME){
-        pressButton(lastPressedButton, PressTime.short);
+        pressButton(lastPressedButton, PressTime::shortPress);
       }
+      // This must be a long press. Run the long press for the previously pressed button.
       // reset the PressTime after recognizing that we just released a long press.
       else {
+        pressButton(lastPressedButton, PressTime::longPress);
         pressTime = 0;
         lastPressedTime = 0;     
       }
     }
-    lastPressedButton = pressedButton
+    lastPressedButton = pressedButton;
   
 }
