18dac8915e4ed6e470866edc06982c2131428878
Aarthi Gurusami <aarthi.gurusami@gmail.com>
2023-04-13 12:57:30 -0700

    Add hsl server cb

--- main/light_hsl_server_model.c
+++ main/light_hsl_server_model.c
@@ -1,6 +1,7 @@
 #ifndef _LIGHT_HSL_SERVER_MODEL_GUARD
 #define _LIGHT_HSL_SERVER_MODEL_GUARD
 #include "esp_ble_mesh_lighting_model_api.h"
+#include "ble_responses.c"
 #include "cobra_colors.h"
 #include "cobra_process.h"
 #include "cobra_queue.c"
